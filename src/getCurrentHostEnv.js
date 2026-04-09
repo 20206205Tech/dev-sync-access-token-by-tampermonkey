56ed40f3c4a09c55934058ba234ef6a7eda16139
VuVanNghia <vuvannghia.work@gmail.com>
2026-04-09 16:35:41 +0700

    Update repository URLs and host environment checks in scripts

--- src/getCurrentHostEnv.js
+++ src/getCurrentHostEnv.js
@@ -2,8 +2,8 @@ import { ENV_DEV, ENV_PROD } from "./config";
 
 export function getCurrentHostEnv() {
   const host = window.location.hostname;
-  if (host.includes("toeichust.local")) return ENV_DEV;
-  if (host.includes("toeichust.me")) {
+  if (host.includes("20206205.local")) return ENV_DEV;
+  if (host.includes("20206205.tech")) {
     return host.startsWith("dev-") ? ENV_DEV : ENV_PROD;
   }
   return null;
--- src/header.js
+++ src/header.js
@@ -4,13 +4,13 @@ export const HEADER = `// ==UserScript==
 // @namespace    VuVanNghia
 // @name          Dev Sync Access Token By Tampermonkey
 // @description  Tampermonkey script for Dev Sync Access Token By Tampermonkey
-// @homepageURL  https://github.com/ToeicHUST/dev-sync-access-token-by-tampermonkey
-// @updateURL    https://github.com/ToeicHUST/dev-sync-access-token-by-tampermonkey/raw/build/dist/dev-sync-access-token-by-tampermonkey.user.js
-// @downloadURL  https://github.com/ToeicHUST/dev-sync-access-token-by-tampermonkey/raw/build/dist/dev-sync-access-token-by-tampermonkey.user.js
+// @homepageURL  https://github.com/20206205tech/dev-sync-access-token-by-tampermonkey
+// @updateURL    https://github.com/20206205tech/dev-sync-access-token-by-tampermonkey/raw/build/dist/dev-sync-access-token-by-tampermonkey.user.js
+// @downloadURL  https://github.com/20206205tech/dev-sync-access-token-by-tampermonkey/raw/build/dist/dev-sync-access-token-by-tampermonkey.user.js
 // @match        https://www.youtube.com/*
-// @match        *://*.toeichust.me/*
-// @match        *://*.toeichust.local/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=toeichust.me
+// @match        *://*.20206205.tech/*
+// @match        *://*.20206205.local/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=20206205.tech
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        unsafeWindow
