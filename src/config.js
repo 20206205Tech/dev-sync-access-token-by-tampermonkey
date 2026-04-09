88782a286347407c83f45466585d16ca8766906b
VuVanNghia <vuvannghia.work@gmail.com>
2026-04-09 17:17:13 +0700

    supabase

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const STORAGE_KEY = "authorized";
 export const ENV_DEV = "DEV";
 export const ENV_PROD = "PROD";
 export const ISS_MAP = {
-  "https://fcpszufggnlztfglyysr.supabase.co/auth/v1": ENV_DEV,
-  "https://umcfsqjxpmawkzwuhwdp.supabase.co/auth/v1": ENV_PROD,
+  "https://cmtvbkvxwspkdstrcrfm.supabase.co/auth/v1": ENV_DEV,
+  "https://subllhhvspwaseobtysd.supabase.co/auth/v1": ENV_PROD,
 };
